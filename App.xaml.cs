d5494e2cd5b96cd74727a474c4a0a89107beeb87
romanokeser <romanokeser@gmail.com>
2024-03-04 09:34:35 +0100

    Update

--- App.xaml.cs
+++ App.xaml.cs
@@ -2,14 +2,13 @@
 using System.Configuration;
 using System.Data;
 using System.Windows;
-using Application = System.Windows.Application;
 
 namespace MouseTrackingV2
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : System.Windows.Application
     {
         protected override void OnStartup(StartupEventArgs e)
         {
